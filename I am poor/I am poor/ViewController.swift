d32b42633790f34829468a47c4dc5c306ac2e713
samh06 <54530346+samh06@users.noreply.github.com>
2020-01-09 19:26:55 +1000

    I am poor

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Samuel Heinz on 7/1/20.
+//  Copyright © 2020 Samuel Heinz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
